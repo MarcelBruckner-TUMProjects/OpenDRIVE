cde1f06668bfee9e8a20e01ba8e91509422bae7f
brucknem <brucknem>
2021-06-24 22:32:12 +0200

    Tests running even when no standard is given

--- examples/basicOpendrive16Loading.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by brucknem on 24.06.21.
-//
-#include "opendrive_1_6/opendrive_16_core.hxx"
-
-int main(int argc, char **argv) {
-    *simulation::standard::opendrive_schema::OpenDRIVE_("../misc/test_map_opendrive_1_6.xodr",
-                                                        ::xml_schema::flags::dont_validate);
-}
